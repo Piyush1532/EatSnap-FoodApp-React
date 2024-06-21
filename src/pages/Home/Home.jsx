d463a257f9fececac30396365825e609a0fadeeb
Piyush1532 <piyushadav503@gmail.com>
2024-06-21 14:45:40 +0530

    added more components

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./home.css"
 import Header from '../../Components/Header/Header'
+import Exploremenu from '../../Components/Exploremenu/Exploremenu'
+import Fooddisplay from '../../Components/Fooddisplay/Fooddisplay'
 const Home = () => {
+
+const [category, setCategory] = useState("All")
+
+
   return (
     <div>
       <Header/>
+      <Exploremenu category={category} setCategory={setCategory}/>
+      <Fooddisplay category={category}/>
     </div>
   )
 }
