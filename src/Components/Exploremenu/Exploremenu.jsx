2f02055a9c1038d81b25b87c5b418efbb5aaeaec
Piyush1532 <piyushadav503@gmail.com>
2024-06-24 15:44:53 +0530

    fixed bug

--- src/Components/Exploremenu/Exploremenu.jsx
+++ src/Components/Exploremenu/Exploremenu.jsx
@@ -10,7 +10,7 @@ const Exploremenu = ({category,setCategory}) => {
       <div className="exploremenu-list">
         {menu_list.map((item,index)=>{
            return(
-            <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} className='exploremenu-list-item' id={index}>
+            <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} className='exploremenu-list-item' key={index}>
              <img className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
              <p>{item.menu_name}</p>
             </div>
