70e087df11a07568e0b0e306eaf29fc265228d90
Piyush1532 <piyushadav503@gmail.com>
2024-06-27 15:26:11 +0530

    component ready

--- src/Components/Appdownload/Appdownlaod.jsx
+++ src/Components/Appdownload/Appdownlaod.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import "./appdownload.css"
+import { assets } from '../../assets/assets'
 const Appdownlaod = () => {
   return (
-    <div>
-      
+    <div className='appdownload' id='appdownload'>
+    <p>For Better Experience Download <br /> EatSnap App</p>
+    <div className="app-download-platform">
+      <img src={assets.play_store} alt="" />
+      <img src={assets.app_store}alt="" />
+    </div>
     </div>
   )
 }
