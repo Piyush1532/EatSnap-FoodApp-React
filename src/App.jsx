6afecbf47793463c42c6386657e2261ae3692318
Piyush1532 <piyushadav503@gmail.com>
2024-07-01 12:10:26 +0530

    added login pop up state and prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import Placeorder from './pages/Placeorder/Placeorder'
 import Footer from './Components/Footer/Footer'
+import Loginpopup from './Components/Loginpopup/Loginpopup'
 
 const App = () => {
+
+const [showLogin, setShowLogin] = useState(false)
+
   return (
-
     <>
+    {showLogin?<Loginpopup setShowLogin={setShowLogin}/>:<></>}
       <div className='app'>
-      <Navbar/>
-
+      <Navbar setShowLogin={setShowLogin}/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/cart' element={<Cart/>}/>
