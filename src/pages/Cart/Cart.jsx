39edbcea49fdc8663ccdb456837ad108104131ee
Piyush1532 <piyushadav503@gmail.com>
2024-07-02 13:54:04 +0530

    added cart page component and functionality

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import "./cart.css"
+import { StoreContext } from '../../Context/Storecontext'
 const Cart = () => {
+ 
+  const {cartItems,food_list,removeFromCart}=useContext(StoreContext)
+
+
   return (
-    <div>
-      
+    <div className='cart'>
+      <div className="cart-items">
+       <div className="cart-items-title">
+        <p>Items</p>
+        <p>Title</p>
+        <p>Price </p>
+        <p>Quantity</p>
+        <p>Total</p>
+        <p>Remove</p>
+       </div>
+       <br />
+       <hr />
+       {food_list.map((item,index)=>{
+         if(cartItems[item._id]>0){
+          return(
+            <div>
+    <div className="cart-items-title cart-items-item" key={index}>
+            <img src={item.image} alt="" />
+            <p>{item.name}</p>
+            <p>${item.price}</p>
+            <p>{cartItems[item._id]}</p>
+            <p>${item.price*cartItems[item._id]}</p>
+            <p className='cross' onClick={()=>removeFromCart(item._id)}>X</p>
+            </div>
+            <hr />
+            </div>
+          )
+         }
+       })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart total</h2>
+          <div>
+            .
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
