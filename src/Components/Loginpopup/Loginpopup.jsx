66f3d3eb9b127c1798466d750ca01c3d5f541abb
Piyush1532 <piyushadav503@gmail.com>
2024-07-02 13:56:06 +0530

    fixed structure and design

--- src/Components/Loginpopup/Loginpopup.jsx
+++ src/Components/Loginpopup/Loginpopup.jsx
@@ -8,29 +8,24 @@ const [currentState,setCurrentState]=useState("Login")
 
   return (
     <div className='loginpop-up'>
-     <div className="login-popup-container">
-      <form className='login-popup-form'>
-        <div className="login-popup-title">
-          <h2>{currentState}</h2>
-          <img  onClick={()=>setShowLogin(false)}  src={assets.cross_icon}  />
-        </div>
-        <div className="login-popup-input">
-        {currentState==="Login"?<></>: <input type="text" placeholder='Your Name' required />}
-          <input type="email"  placeholder='Your Email' required/>
-          <input type="password" placeholder='Your Password' required /> 
-        </div>
-
-        
-          <div className="login-pop-condition">
-            <input type="checkbox" required/>
-            <p>By continuing ,I agree to the terms of use & privacy policy </p>
-          </div>
-          {currentState==="Login"?<p>Create a new account? <span onClick={()=>setCurrentState("sign up")}>Click Here</span></p>:
-            <p>Alerady have an account <span onClick={()=>setCurrentState("Login")}>Login here</span></p>}
-
-      </form>
-            <button>{currentState==="sign up"?"create account":"Login"}</button>
-     </div>
+<form className="login-popup-container">
+  <div className="login-popup-title">
+    <h2>{currentState}</h2>
+    <img onClick={()=>setShowLogin(false)}  src={assets.cross_icon} alt="" />
+  </div>
+  <div className="login-popup-input">
+    {currentState==="Login"?<></>:<input type="text" placeholder='Your Name'  required/>}
+  
+    <input type="email" placeholder='Your Email'  required/>
+    <input type="password" placeholder='Password'  required/>
+  </div>
+  <button>{currentState==="Sign up"?"Create Account":"Login"}</button>
+  <div className="login-popup-condition">
+    <input type="checkbox"  required/>
+    <p>By continuing ,I agree to the terms of use & privacy policy</p>
+  </div>
+  {currentState==="Login"?<p>Create a new account? <span onClick={()=>setCurrentState("Sign up")}>Click Here</span> </p>:<p>Already have an account <span onClick={()=>setCurrentState("Login")}>Login Here</span></p>}
+</form>
     </div>
   )
 }
