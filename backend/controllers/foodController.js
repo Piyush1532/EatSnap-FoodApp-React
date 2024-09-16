f516b47c554174fc426bf92a3729b3ab8a21d32d
Piyush1532 <piyushadav503@gmail.com>
2024-09-16 14:37:06 +0530

    added all food &remove food functionality

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -26,5 +26,33 @@ res.json({success:false,message:"error"})
 }
 
 }
+  
+// all food List
 
-export {addFood}
+
+const listFood=async(req,res)=>{
+try {
+    const foods=await foodModel.find({})
+    res.json({success:true,data:{foods}})
+} catch (error) {
+    console.log(error);
+    res.json({success:false,message:error})
+}
+}
+
+
+//remove food item
+const removeFood=async (req,res) => {
+    try {
+        const food=await foodModel.findById(req.body.id)
+        fs.unlink(`uploads/${food.image}`,()=>{})
+
+        await foodModel.findByIdAndDelete(req.body.id)
+        res.json({success:true,message:"food removed"})
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error})
+    }
+}
+
+export {addFood,listFood,removeFood}
