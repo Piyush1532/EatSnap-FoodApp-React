6b11612bef91a3632ce574a4cc10ba69cec5ed3f
Piyush1532 <piyushadav503@gmail.com>
2024-09-19 15:09:30 +0530

    mongo altlas db

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://PiyuAdav:Piyush1234@clustereatsnap.vi5sb.mongodb.net/?"
+      "mongodb+srv://PiyuAdav:Piyush1234@clustereatsnap.vi5sb.mongodb.net/Eat-snap"
     );
     console.log("Database connected");
   } catch (error) {
