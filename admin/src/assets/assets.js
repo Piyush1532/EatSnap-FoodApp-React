80bce1c1e6eefbc83a17b20d0ff9068baaa6aae9
Piyush1532 <piyushadav503@gmail.com>
2024-09-25 14:44:48 +0530

    updated

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,9 +1,9 @@
 import logo from "./logo.png"
 import  add_icon from "./add_icon_white.png"
-import order_icon from "order-27.png"
-import profile_img from "./user_icon"
-import upload_icon from "upload_icon.png"
-import parcel_icon from "parcel_icon.png"
+import order_icon from "./order-27.png"
+import profile_img from "./user_icon.png"
+import upload_icon from "./upload_icon.webp"
+import parcel_icon from "./parcel_icon.png"
 
 
 export const assets ={
