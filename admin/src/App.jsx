c59256f45e7dc83daf379993fef0c2288d715582
Piyush1532 <piyushadav503@gmail.com>
2024-09-25 14:42:40 +0530

    added routes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import Navbar from './Components/Navbar/navBar'
+import Sidebar from './Components/Sidebar/sideBar'
+import { Route, Routes } from 'react-router-dom'
+import Add from './Pages/Add/Add'
+import List from './Pages/List/List'
+import Order from './Pages/Orders/Order'
 
 const App = () => {
   return (
     <div>
-      
+<Navbar/>
+<hr />
+<div className="app-content">
+  <Sidebar/>
+<Routes>
+  <Route path='/add' element={<Add/>}/>
+  <Route path='/list' element={<List/>}/>
+  <Route path='/order' element={<Order/>}/>
+</Routes>
+
+</div>
     </div>
   )
 }
